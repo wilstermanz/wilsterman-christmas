5070e7bcb1b9b40d304a82772a40da1d4dc73db0
Zach Wilsterman <wilstermanz@gmail.com>
2024-10-09 00:01:23 -0500

    Added Annie's match

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,21 +5,21 @@ import streamlit as st
 
 random.seed(0)
 
-# Real actual family
-family = [
-    'Tom',
-    'Vickie',
-    'Zach',
-    'Katie',
-    'Berto',
-    'Annie',
-    'Nick',
-    'Bekah',
-    'Lilly'
-]
+# # Real actual family
+# family = [
+#     'Tom',
+#     'Vickie',
+#     'Zach',
+#     'Katie',
+#     'Berto',
+#     'Annie',
+#     'Nick',
+#     'Bekah',
+#     'Lilly'
+# ]
 
-# # Test family
-# family = ['Alice', 'Brian', 'Catherine', 'David', 'Ella', 'Frank', 'Grace', 'Henry', 'Ivy']
+# Test family
+family = ['Alice', 'Brian', 'Catherine', 'David', 'Ella', 'Frank', 'Grace', 'Henry', 'Ivy']
 
 possible_matches = []
 for giver in family:
@@ -32,18 +32,19 @@ for match in possible_matches:
     hash = md5(str(match).encode()).hexdigest()
     matches_encoded[hash] = match
 
-# Real actual known matches
-known_matches = [
-    'd6756d8309907c3acde021b8cae3998c'     # Vickie's match
-]
-
-# # test known matches
+# # Real actual known matches
 # known_matches = [
-#     '84060eff6a9ad9282ff5651f59c5f384',
-#     '4b8c7499eb94277d5f4015bf7c8bfaf6',
-#     '7d370a2bd0ccc1119e5d668b54e7e2ff',
-#     'c81f715ca643d2ba7f9b55824702354a'
-#     ]
+#     'd6756d8309907c3acde021b8cae3998c',     # Vickie's match
+#     '6e0e6f62753a0e4b27ed83f485c08c86'      # Annie's match
+# ]
+
+# test known matches
+known_matches = [
+    '84060eff6a9ad9282ff5651f59c5f384',
+    '4b8c7499eb94277d5f4015bf7c8bfaf6',
+    '7d370a2bd0ccc1119e5d668b54e7e2ff',
+    'c81f715ca643d2ba7f9b55824702354a'
+    ]
 
 givers = family.copy()
 recipients = family.copy()
@@ -70,5 +71,5 @@ if __name__ == '__main__':
         submitted = st.form_submit_button("Get my person!")
         if submitted and len(code) == 6:
             for match in known_matches:
-                if code in match:
+                if code == match[::6]:
                     st.success(matches_encoded[match][1])
